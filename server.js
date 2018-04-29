959dfb7cdf401efd372d3d0bd768a107438d9564
gitterxo <dorinenache@fastmail.com>
2018-04-29 21:22:10 +0300

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs'); // handlebars - e un fel de jade
 const fs = require('fs');
+const port = process.env.PORT || 3000; // heroku o sa isi puna singur; daca nu exista punem 3000
 
 var app = express();
 
@@ -73,6 +74,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000');
 });
